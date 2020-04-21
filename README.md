2555e14b5afd494a41f04258df861f0bd62b13ef
Salil Chhetri <salil@shiftrock.com>
2020-04-21 10:06:06 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# salilchhetri.github.io
---
+++ precache-manifest.d608f38875cf4f596285693d3fff635e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67b0e963a7001d1b0c7f2931a18a74d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7eec36fdfd50b496977",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "16e97df599e385405582",
+    "url": "/static/css/main.3567096c.chunk.css"
+  },
+  {
+    "revision": "f7eec36fdfd50b496977",
+    "url": "/static/js/2.928124f3.chunk.js"
+  },
+  {
+    "revision": "16e97df599e385405582",
+    "url": "/static/js/main.9981b9a4.chunk.js"
+  },
+  {
+    "revision": "e07d80c3afec08c0bc19",
+    "url": "/static/js/runtime-main.3ad62e09.js"
+  },
+  {
+    "revision": "15c4c6871987e2ce100b2c5bbf51a771",
+    "url": "/static/media/about-purplehaze.15c4c687.png"
+  },
+  {
+    "revision": "f474cc26816e63f73c912da3ccf3df2f",
+    "url": "/static/media/being-soraya.f474cc26.jpg"
+  },
+  {
+    "revision": "093fb77415f7bd9f7e9ac5b6fca29850",
+    "url": "/static/media/full.093fb774.jpg"
+  },
+  {
+    "revision": "8cac5974a300670cd4df28006b7aef8b",
+    "url": "/static/media/full.8cac5974.jpg"
+  },
+  {
+    "revision": "d8ce01a42f5d65e6701b5c615aa03813",
+    "url": "/static/media/full.d8ce01a4.jpg"
+  },
+  {
+    "revision": "14e95f49c4fde0727c0c98d5c08f2042",
+    "url": "/static/media/qiworks.14e95f49.png"
+  },
+  {
+    "revision": "5675b9699141a0f7f215580be4321956",
+    "url": "/static/media/thumb.5675b969.jpg"
+  },
+  {
+    "revision": "57ddcd6d5059ca0632e10f791df8ea9d",
+    "url": "/static/media/thumb.57ddcd6d.jpg"
+  },
+  {
+    "revision": "708a81e24d1ac05cdc6ac1d72a1dfb4b",
+    "url": "/static/media/thumb.708a81e2.jpg"
+  }
+]);
